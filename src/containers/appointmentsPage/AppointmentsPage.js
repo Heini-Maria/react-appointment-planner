e4538d20cf2e0c209a363bee482d76a858719da5
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-11-28 18:03:46 +0100

    finish task 10

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -30,6 +30,7 @@ export const AppointmentsPage = ({ appointments, addAppointment, contacts }) => 
       <section>
         <h2>Add Appointment</h2>
         <AppointmentForm 
+        contacts={contacts}
         title= {title}
         setTitle= {setTitle}
         date= {date}
