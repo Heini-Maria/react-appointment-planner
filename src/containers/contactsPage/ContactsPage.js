faa01786682dd8af3f9aa51b1d9e7402d2819978
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-11-28 15:59:58 +0100

    finished 3

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import { ContactForm } from '../../components/contactForm/ContactForm';
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+ const [name, setName] = useState('');
+ const [phone, setPhone] = useState('');
+ const [email, setEmail] = useState('');
+ const [dublicate, setDublicate] =useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,21 +18,45 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if(dublicate === false) {
+    addContact(name, phone, email);
+    setName("");
+    setPhone("");
+    setEmail("");
+   }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+ useEffect(() => {
+  for(let i=0; i<contacts.length; i++) {
+    if(contacts[i].name === name) {
+      setDublicate(true);
+    };
+  };
+ }, [name])
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm 
+        name={name}
+        setNema={setName}
+        phone={phone}
+        setPhone={setPhone}
+        email={setEmail}
+        handleSubmit={handleSubmit}
+        /> 
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList 
+        contacts={contacts}
+        />
       </section>
     </div>
   );
