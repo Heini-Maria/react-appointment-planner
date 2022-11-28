45e75c5db1dd01d63f5d6b8b00735ee068cf7e80
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-11-28 16:20:04 +0100

    finished task 4

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import { ContactForm } from '../../components/contactForm/ContactForm';
+import { TileList } from '../../components/tileList/TileList';
 
 export const ContactsPage = ({ contacts, addContact }) => {
   /*
@@ -44,10 +45,11 @@ export const ContactsPage = ({ contacts, addContact }) => {
         <h2>Add Contact</h2>
         <ContactForm 
         name={name}
-        setNema={setName}
+        setName={setName}
         phone={phone}
         setPhone={setPhone}
-        email={setEmail}
+        email={email}
+        setEmail={setEmail}
         handleSubmit={handleSubmit}
         /> 
       </section>
