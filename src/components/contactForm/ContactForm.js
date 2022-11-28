ed1475d0c7e6242ac8d41c681764c6b61c8691ce
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-11-28 16:24:51 +0100

    finished task 4

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,35 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <div>
+      <form onSubmit={PaymentResponse.handleSubmit}>
+        <label for='name'>Name</label>
+        <input 
+        type='text'
+        name='name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder='your name'
+        />
+        <label for='phone'>Phone Number</label>
+        <input 
+        type='tel'
+        name='phone'
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        placeholder='+xxx xxxx xxx'
+        pattern= '^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$'
+        />
+        <label for='email'>Email</label>
+        <input 
+        type='email'
+        name='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder='email address'
+        />
+        <input type='submit' value='Add Contact'/>
+      </form>
+    </div>
   );
 };
