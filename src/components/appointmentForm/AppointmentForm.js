53b5db6c6c87b37fcd0179c3241db9387a14a248
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-11-28 17:25:16 +0100

    finish task 8

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from '../../components/contactPicker/ContactPicker';
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +21,32 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <label for='title'>Title</label>
+      <input 
+      type='text'
+      name='title'
+      value= {title}
+      onChange={(e) => setTitle(e.target.value)}
+      placeholder = 'Title for the appointment'
+      />
+      <label for='date'>Date</label>
+      <input 
+      type='date'
+      name='date'
+      value={date}
+      onChange={(e) => setDate(e.target.value)}
+      />
+      <label for='time'>Time</label>
+      <input 
+      type='time'
+      name='time'
+      value={time}
+      onChange= {(e) => setTime(e.target.value)}
+      />
+      <input type='submit' value='Set Appointment'/>
+
+
+    </form>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,53 @@
 import React from "react";
+import { useState } from 'react';
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({ appointments, addAppointment, contacts }) => {
   /*
   Define state variables for 
   appointment info
   */
+ const [title, setTitle] = useState('');
+ const [date, setDate] = useState('');
+ const [time, setTime] = useState('');
+ const [contact, setContact] = useState(contacts.length > 0 ? contacts[0].name : "")
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+   addAppointment(title, date, time, contact);
+    setTitle('');
+    setImmediate('');
+    setTime('');
+    setContact('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+        title= {title}
+        setTitle= {setTitle}
+        date= {date}
+        setDate= {setDate}
+        time= {time}
+        setTime= {setTime}
+        contact= {contact}
+        setContact= {setContact}
+        onSubmit= {handleSubmit}
+
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList 
+        array={appointments}
+        />
       </section>
     </div>
   );
